e83dcb059ad2cf4243457a0defc9c06ca5a085fa
RegisWeill <regis_weill@hotmail.com>
2019-05-10 19:55:18 +0200

    CRUD ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,23 +9,29 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-
+    article = Article.new(article_params)
+    article.save
+    redirect_to articles_path
   end
 
   def edit
-
+    @article = Article.find(params[:id])
   end
 
   def update
-
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy
-
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
