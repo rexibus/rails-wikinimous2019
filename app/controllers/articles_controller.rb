6a18df81fef8fecb17123188b6cc59955aa2b79c
RegisWeill <regis_weill@hotmail.com>
2019-05-10 19:01:07 +0200

    index show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,38 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def article_params
+    #strong params for create and update
+    params.require(:article).permit(:title, :content)
+  end
+
 end
