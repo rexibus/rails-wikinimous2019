6dad2ffa53a55a30ccbd9c630110576fad3c5ae2
RegisWeill <regis_weill@hotmail.com>
2019-05-11 02:42:24 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Article.destroy_all
+
 10.times do
   Article.create(
     title: Faker::Lorem.word,
